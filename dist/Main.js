b10fa38a227e1c66d87700de1922635649d78aa8
Kyle Rector <krtools@users.noreply.github.com>
2022-11-23 23:00:29 -0500

    Remove dist

--- dist/Main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const moment = require("moment");
-const fs = require("fs");
-!(async () => {
-    console.log(moment().format('MM/DD/YYYY'));
-    console.log(fs);
-})();
-//# sourceMappingURL=Main.js.map
