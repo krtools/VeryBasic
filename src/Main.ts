4f309b6903ebbfeb1d162eaee9fdf578d0431b5d
Kyle Rector <krtools@users.noreply.github.com>
2022-11-23 22:58:35 -0500

    Initial

---
+++ src/Main.ts
@@ -0,0 +1,8 @@
+import * as minimist from 'minimist';
+import * as moment from 'moment';
+import * as fs from 'fs';
+
+!(async () => {
+    console.log(moment().format('MM/DD/YYYY'));
+    console.log(fs);
+})();
